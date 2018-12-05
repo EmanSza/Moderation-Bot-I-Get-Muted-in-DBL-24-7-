b0ccedc4d51958bf172328248d41618ae46d550d
dh48dji59fji4 <44507685+EmanSza@users.noreply.github.com>
2018-12-05 14:17:15 -0800

    Create help1.js

---
+++ commands/help1.js
@@ -0,0 +1,26 @@
+const Discord = require("discord.js");
+const botconfig = require("../botconfig.json");
+
+module.exports.run = async (bot, message, args) => {
+
+    let prefix = botconfig.prefix;
+    if (!message.content.startsWith(prefix)) return;
+    let bicon = bot.user.displayAvatarURL;
+    let botembed = new Discord.RichEmbed()
+        .setThumbnail(bicon)
+        .addField("Moderation", "Commands")
+        .addField("Ban", "Bans a Member from the Server")
+    .addField("Kick", "Kicks a Member From The Server")
+    .addField("Tempmute", "Tempmute Someone From Talking")
+    .addField("Mute", "Mute someone from Talking")
+    .addField("Unmute", "Unmute a member from Talking")
+    .addField("Warn", "Warn someone for breaking the rules")
+        .setColor('RANDOM');
+      
+
+    message.channel.send(botembed);
+}
+
+module.exports.help = {
+    name: "help1"
+}

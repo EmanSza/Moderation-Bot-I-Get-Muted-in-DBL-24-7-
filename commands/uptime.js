179b3596f6c04fef4aa1461e91ce8cec825ba4c7
dh48dji59fji4 <44507685+EmanSza@users.noreply.github.com>
2018-12-05 14:39:25 -0800

    Update uptime.js

--- commands/uptime.js
+++ commands/uptime.js
@@ -5,7 +5,7 @@ module.exports.run = (bot, message, args) => {
   let prefix = botconfig.prefix;
 if (!message.content.startsWith(prefix)) return;
   var hrs = Math.round(bot.uptime / (1000 * 60 * 60)) + " hour(s),"
-  var mins = " " + Math.round(bot.uptime / (1000 * 60)) % 60 + " minute(s), "
+  var mins = " " + Math.round(bot.uptime / (1000 * 60)) % 60 + " minute(s), " 
   var sec = Math.round(bot.uptime / 1000) % 60 + " second(s)"
   if (hrs == "0 hour(s),") hrs = ""
   if (mins == " 0 minute(s), ") mins = ""

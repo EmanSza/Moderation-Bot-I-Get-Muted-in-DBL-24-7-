c2005a90cffda01d3d1123d8394b99f3b4849d69
dh48dji59fji4 <44507685+EmanSza@users.noreply.github.com>
2018-12-05 14:27:29 -0800

    Update kick.js

--- commands/kick.js
+++ commands/kick.js
@@ -6,7 +6,7 @@ module.exports.run = async (bot, message, args) => {
     let prefix = botconfig.prefix;
 if (!message.content.startsWith(prefix)) return;
     let kUser = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
-    if(!kUser) return message.channel.send("Can't find user!");
+    if(!kUser) return message.channel.send("Can't find This user!");
     let kReason = args.join(" ").slice(22);
     if(!message.member.hasPermission("KICK_MEMBERS")) return message.channel.send("You Dont Have KICK_MEMBERS Permission");
     if(kUser.hasPermission("KICK_MEMBERS")) return message.channel.send("You Cant Kick This Member");

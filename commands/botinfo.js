5c0f5691ef7f7e6391dc5326e5c20dfa059fa896
dh48dji59fji4 <44507685+EmanSza@users.noreply.github.com>
2018-12-05 14:21:00 -0800

    Create botinfo.js

---
+++ commands/botinfo.js
@@ -0,0 +1,21 @@
+const Discord = require("discord.js");
+const botconfig = require("../botconfig.json");
+
+module.exports.run = async (bot, message, args) => {
+
+    let prefix = botconfig.prefix;
+if (!message.content.startsWith(prefix)) return;
+    let bicon = bot.user.displayAvatarURL;
+    let botembed = new Discord.RichEmbed()
+    .setDescription("Bot Information")
+    .setColor("#15f153")
+    .setThumbnail(bicon)
+    .addField("Bot Name", bot.user.username)
+    .addField("Created On", bot.user.createdAt);
+
+    message.channel.send(botembed);
+}
+
+module.exports.help = {
+  name:"botinfo"
+}

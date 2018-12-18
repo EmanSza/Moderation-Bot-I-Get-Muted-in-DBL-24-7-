544883b91a729597a5eb50fae940798215a57d53
EmanSza <eszabadka@hotmail.ca>
2018-12-18 12:12:02 -0500

    Update purge.js

--- commands/purge.js
+++ commands/purge.js
@@ -1,10 +1,12 @@
-const Discord = require("discord.js");
+const botconfig = require("../botconfig.json");
 
-module.exports.run = async (bot, message, args, messages) => {
+module.exports.run = async (bot, message, args) => {
+  let prefix = botconfig.prefix;
+if (!message.content.startsWith(prefix)) return;
 
   const deleteCount = parseInt(args[0], 10);
-  if (!message.member.hasPermission("MANAGE_MESSAGES")) return message.reply("No no no.");
-    if (!args[0] || args[0 == "help"]) return message.reply(`Please Usage: rens!prefix <new prefix here>"`);
+  if (!message.member.hasPermission("MANAGE_MESSAGES")) return message.reply("You Do Not Have MANAGE_MESSAGES Permmission");
+    if (!args[0] || args[0 == "help"]) return message.reply(`Please Usage:nb.purge "`);
     
     if(!deleteCount || deleteCount < 2 || deleteCount > 100)
       return message.reply("Please provide a number between 2 and 100 for the number of messages to delete.");
@@ -19,10 +21,10 @@ module.exports.run = async (bot, message, args, messages) => {
     .addField("Executor", `<@${message.author.id}>`)
     .addField("Purge", `${args[0]}`)
     .addField("Deleted", `${args[0]}`)
-    .setFooter("Bot Version 1.0.0", bot.user.displayAvatarURL);
+    .setFooter("Bot Version 1.0.2", bot.user.displayAvatarURL);
 
-    let purgeChannel = message.guild.channels.find(`name`, "mod-logs");
-    if(!purgeChannel) return message.channel.send("Can't find mod-logs channel.");
+    let purgeChannel = message.guild.channels.find(`name`, "mod-log");
+    if(!purgeChannel) return message.channel.send("Can't find mod-log channel.");
 
     purgeChannel.send(purgeEmbed);
 

e54ec674ca28d56738763ede057bf00204ee0e98
EmanSza <eszabadka@hotmail.ca>
2018-12-24 15:19:10 -0500

    Update index.js

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const Discord = require("discord.js");
 const fs = require("fs");
 const bot = new Discord.Client({disableEveryone: true});
 bot.commands = new Discord.Collection();
+//const dbl = new DBL('Your discordbots.org token', client);
 
 bot.login(process.env.token);
 
@@ -72,4 +73,6 @@ bot.on('guildMemberremove', member => {
   // Send the message, mentioning the member
   channel.send(`${member} left the Server`);
 });
-
+ /*dbl.on('posted', () => {
+  console.log('Server count posted!');
+})*/ 

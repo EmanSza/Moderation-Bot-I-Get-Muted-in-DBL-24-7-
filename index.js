30f0aea433b105700253e9f7ce0855e99ea670ed
EmanSza <eszabadka@hotmail.ca>
2019-01-15 13:55:07 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,143 @@
+const botconfig = require("./botconfig.json");
+const Discord = require("discord.js");
+const fs = require("fs");
+const bot = new Discord.Client({disableEveryone: true});
+bot.commands = new Discord.Collection();
+const client = new Discord.Client();
+const CHANNEL = 'log';
+const DBL = require("dblapi.js");
+const dbl = new DBL('eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjUyMzM3NTQ1MjY2OTA4MzY1NSIsImJvdCI6dHJ1ZSwiaWF0IjoxNTQ3NDkyMzI3fQ.NHv_8gubRlhFzhddGZ54_sGvtAhkKNQ4kP1SCzLuADc', client);
 
+// Optional events
+dbl.on('posted', () => {
+  console.log('Server count posted!');
+})
+
+dbl.on('error', e => {
+ console.log(`Oops! ${e}`);
+})
+
+bot.login(`NTIzMzc1NDUyNjY5MDgzNjU1.Dx5oVg.8MJbS5H-3jHBeGgiZAni9SSRARo`);
+
+fs.readdir("./commands/", (err, files) => {
+
+  if(err) console.log(err);
+  let jsfile = files.filter(f => f.split(".").pop() === "js");
+  if(jsfile.length <= 0){
+    console.log("Couldn't find commands.");
+    return;
+  }
+
+  jsfile.forEach((f, i) =>{
+    let props = require(`./commands/${f}`);
+    console.log(`${f} loaded!`);
+    bot.commands.set(props.help.name, props);
+  
+  });
+});
+bot.on("ready", async () => {
+
+  console.log(`${bot.user.username} is online on ${bot.guilds.size} servers!`);
+  bot.user.setActivity(` Added GLobal Se Members :D` , {type: "WATCHING"});
+
+});
+
+//bot.on('ready', () => {
+   // bot.user.setStatus('available')
+    //bot.user.setPresence({
+      //  game: {
+        //    name: ` ${guild.memberCount} Members :D`,
+          //  type: "watching",
+            //url:"https://www.twitch.tv/monstercat"
+        //}
+    //});
+//});
+
+
+
+bot.on("message", async message => {
+  if(message.author.bot) return;
+  if(message.channel.type === "dm") return;
+
+  let prefix = botconfig.prefix;
+  let messageArray = message.content.split(" ");
+  let cmd = messageArray[0];
+  let args = messageArray.slice(1);
+  let commandfile = bot.commands.get(cmd.slice(prefix.length));
+  if(commandfile) commandfile.run(bot,message,args);
+// }
+
+});
+bot.on('channelCreate', async channel => {
+
+  console.log(`${channel.name} has been created.`);
+
+if (channel.type != 'text') return;
+  let sChannel = channel.guild.channels.find(ch => ch.name === 'mod-log');
+  sChannel.send(`The channel ${channel} has been created`);
+
+});
+bot.on('guildMemberAdd', member => {
+  // Send the message to a designated channel on a server:
+  const channel = member.guild.channels.find(ch => ch.name === 'mod-log');
+  // Do nothing if the channel wasn't found on this server
+  if (!channel) return;
+  // Send the message, mentioning the member
+  channel.send(`${member} joined the Server`);
+});
+bot.on('guildMemberremove', member => {
+  // Send the message to a designated channel on a server:
+  const channel = member.guild.channels.find(ch => ch.name === 'mod-log');
+  // Do nothing if the channel wasn't found on this server
+  if (!channel) return;
+  // Send the message, mentioning the member
+  channel.send(`${member} left the Server`);
+});
+ /*dbl.on('posted', () => {
+  console.log('Server count posted!');
+})*/ 
+bot.on('guildBanAdd', function(guild, user) {
+
+    //log to console
+    console.log('[' + guild.name + '][BAN] ' + user.username + '#' + user.discriminator);
+
+    //post in the guild's log channel
+    var log = guild.channels.find(ch => ch.name === 'global-mod-log');
+    if (log != null)
+        log.sendMessage('**[Banned]** ' + user);
+
+});
+
+//user has been unbanned
+bot.on('guildBanRemove', function(guild, user) {
+
+    //log to console
+    console.log('[' + guild.name + '][UNBAN] ' + user.username + '#' + user.discriminator);
+
+    //post in the guild's log channel
+    var log = guild.channels.find(ch => ch.name === 'global-mod-log');
+    if (log != null)
+        log.sendMessage('**[Unbanned]** ' + user);
+
+});
+bot.on('guildMemberUpdate', function(guild, oldMember, newMember) {
+
+    //declare changes
+    var Changes = {
+        unknown: 0,
+        addedRole: 1,
+        removedRole: 2,
+        username: 3,
+        nickname: 4,
+        avatar: 5
+    };
+    var change = Changes.unknown;
+
+    //check if roles were removed
+    var removedRole = '';
+    oldMember.roles.every(function(value) {
+        if(newMember.roles.find('id', value.id) == null) {
+            change = Changes.removedRole;
+            removedRole = value.name;
+        }
+})

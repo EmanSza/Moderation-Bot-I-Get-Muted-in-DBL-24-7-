f4c2fd36c9aa97fbe7b458f20fdb8d7f91668f46
EmanSza <eszabadka@hotmail.ca>
2019-01-16 11:12:29 -0500

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -12,7 +12,7 @@ module.exports.run = async (bot, message, args) => {
         .addField("help1", "Moderation Commands")
     .addField("help2", "Extra  Commands")
     .addField("ModLogs","Create A Channel Called Mod-logs to get bot logs")
-    .addField("GlobalLogs", "Create a Channel Called Global-mod-logs For Global Logs")
+    .addField("GlobalLogs", "Create a Channel Called Global-mod-log For Global Logs")
     .addField("Offical Support Server", "https://discord.gg/jnUnXTX")
         .setColor('RANDOM');
 

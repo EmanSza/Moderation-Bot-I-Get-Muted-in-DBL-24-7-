fed4699b71ea132ef5a4a2054720f0dbba610a39
EmanSza <eszabadka@hotmail.ca>
2019-01-17 14:51:39 -0500

    Update index.js

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ dbl.on('error', e => {
 })
 bot.on('ready', () => {
     setInterval(() => {
-        dbl.postStats(client.guilds.size, client.shards.total);
+        dbl.postStats(client.guilds.size);
     }, 1);
 });
 dbl.getStats("523375452669083655").then(stats => {

0a73491045e2d4735ffd74b2f9a3a97211be6fa2
EmanSza <eszabadka@hotmail.ca>
2020-05-22 16:42:54 -0400

    Update say.js

--- commands/say.js
+++ commands/say.js
@@ -1,10 +1,14 @@
 //519861424017768451
 
 const discord = require("discord.js")
+const botconfig = require("../botconfig.json");
+
 
 exports.run = (bot,msg,args) =>{
   msg.delete();
   if(msg.author.id !== "519861424017768451") return msg.reply('Owner only');
+  let prefix = botconfig.prefix;
+if (!message.content.startsWith(prefix)) return;
   if(args <=0) return msg.reply(`Say what ?`);
   let content = args.join(" ");
   let embed = new discord.RichEmbed().setTitle(content).setColor("0x3d3d29");

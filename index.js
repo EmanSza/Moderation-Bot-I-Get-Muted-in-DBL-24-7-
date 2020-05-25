3547dea39ba602a46f1260ae7145b049cc1aeffc
EmanSza <eszabadka@hotmail.ca>
2020-05-25 18:45:36 -0400

    Fixed Line 66

--- index.js
+++ index.js
@@ -63,7 +63,6 @@ bot.on("message", async message => {
   if(message.author.bot) return;
   if(message.channel.type === "dm") return;
   let prefix = botconfig.prefix;
-  let prefix = botconfig.prefix;
   if(!message.content.startsWith(prefix)) return
   let messageArray = message.content.split(" ");
   let cmd = messageArray[0].ToLowerCase;
